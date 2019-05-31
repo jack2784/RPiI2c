ac5cc09e23d8729ee7e64533662387c8476db5f4
jack2784 <49051469+jack2784@users.noreply.github.com>
2019-05-31 15:56:00 +1000

    Add files via upload

---
+++ 8-1.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
+   
+Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
+
+void setup(void) 
+{
+  Serial.begin(9600);
+  Serial.println("Light Sensor"); Serial.println("");
+}
+
+void loop(void) 
+{  
+
+  sensors_event_t event;
+  tsl.getEvent(&event);
+ 
+  if (event.light)
+  {
+    Serial.print(event.light); Serial.println(" lux");
+  }
+  delay(1000);
+}
